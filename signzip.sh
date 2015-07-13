cfe27cdd28697141aa3ba678ed2a14beec5685ec
GrinningFerret <luis.hage@outlook.de>
2015-07-13 16:47:23 +0200

    Inintial commit
    Signed-off-by: GrinningFerret <luis.hage@outlook.de>

---
+++ signzip.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This script calls the signapk.jar file to sign a flashable update.zip
+# Colour variables
+red='tput setaf 1'		# red
+green='tput setaf 2'            # green
+yellow='tput setaf 3'		# yellow
+blue='tput setaf 4'		# blue
+violet='tput setaf 5'           # violet
+lblue='tput setaf 6'		# lightblue
+grey='tput setaf 7'		# grey
+white='tput setaf 10'		# white
+normal='tput sgr0'		# normal
+if [ ! $1 ]
+then
+$red
+echo -e "No input file defined!"
+$normal
+echo -e "Usage: signzip [FILENAME]"
+else
+echo -e "Signing zip..."
+sleep 2
+java -jar ~/signapk/signapk.jar ~/signapk/testkey.x509.pem ~/signapk/testkey.pk8 $1 "signed_"$1
+md5sum "signed_"$1 > "signed_"$1.md5sum
+$lblue
+echo -e "Package completed: "signed_"$1 "
+$normal
+fi
+
