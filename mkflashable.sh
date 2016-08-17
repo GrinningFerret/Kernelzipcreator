f0d0178641a91c245fab33daca5ed5f58212f8fe
GrinningFerret <luis.hage@outlook.de>
2016-08-17 14:58:23 +0200

    Fix some issues and add clean + help option * Fix file paths with ~ in path * Fix /dev/null name * Sign the temp zip file and don't move it before * Add option to clean the output directory * Add a help option (manual)
    Signed-off-by: GrinningFerret <luis.hage@outlook.de>

--- mkflashable.sh
+++ mkflashable.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 # © 2016 Luis Hagenauer
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for i in "$@"; do
 case $i in
     -k=*|--kernel-path=*)
     KERNEL_PATH="${i#*=}"
+    eval KERNEL_PATH=$KERNEL_PATH
     shift
     ;;
     -o=*|--output-name=*)
     OUTPUT_NAME="${i#*=}"
+    shift
+    ;;
+    -c|--clean)
+    echo "Cleaning output directory"
+    rm -rf $DIR/out/*
+    exit 0;
+    shift
+    ;;
+    -h|--help)
+    echo -e "Usage: mkflashable [options...]\nOptions:\n"
+    echo "   -k, --kernel-path='FILEPATH'  Specify the kernel path"
+    echo "   -o, --output-name='NAME'      Specify the output name"
+    echo "   -c, --clean                   Clean the output directory"
+    echo "   -h, --help                    Display this help message"
+    exit 0;
     shift
     ;;
     *)
@@ -15,17 +32,18 @@ case $i in
   esac
 done
     if [ -z $KERNEL_PATH ] || [ -z $OUTPUT_NAME ]
-    then
-    echo "You have to specifiy the kernel path (-k) and output name (-o).\n"
-    exit 1
-  fi
-    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+      then
+          echo "You have to specifiy the kernel path (-k) and output name (-o)."
+          exit 1
+    fi
+    # Clean
+    rm -f /tmp/$OUTPUT_NAME.zip
     # Workaround for zip not allowing to zip a folder structure without the directories above it
-    pushd $DIR > /dev/nul
+    pushd $DIR > /dev/null
     zip -qj /tmp/$OUTPUT_NAME.zip $KERNEL_PATH
-    zip -ur /tmp/$OUTPUT_NAME.zip META-INF
-    popd > /dev/nul
-    mv /tmp/$OUTPUT_NAME.zip $DIR/out/$OUTPUT_NAME.zip
-    java -jar $DIR/signapk.jar $DIR/testkey.x509.pem $DIR/testkey.pk8 $DIR/out/$OUTPUT_NAME.zip $DIR/out/signed_$OUTPUT_NAME.zip
-    md5sum $DIR/out/signed_$OUTPUT_NAME.zip > $DIR/out/signed_$OUTPUT_NAME.zip.md5sum
+    zip -ur /tmp/$OUTPUT_NAME.zip META-INF > /dev/null
+    popd > /dev/null
+    java -jar $DIR/signapk.jar $DIR/testkey.x509.pem $DIR/testkey.pk8 /tmp/$OUTPUT_NAME.zip $DIR/out/signed_$OUTPUT_NAME.zip && rm /tmp/$OUTPUT_NAME.zip
+    md5=($(md5sum $DIR/out/signed_$OUTPUT_NAME.zip))
+    echo "$md5 $(basename $DIR/out/signed_$OUTPUT_NAME.zip)" > $DIR/out/signed_$OUTPUT_NAME.zip.md5sum
     echo "Output: $DIR/out/signed_$OUTPUT_NAME.zip"
